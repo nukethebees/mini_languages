2bda53a3bd2ff6b55c44e303dee634b7cc1b7f97
Matthew Love <matthewlove94@gmail.com>
2025-06-16 22:03:03 +0100

    Using own vector

--- s_expr_int_calculator/s_expr_int_calculator/s_expr_int_calculator.cpp
+++ s_expr_int_calculator/s_expr_int_calculator/s_expr_int_calculator.cpp
@@ -1,10 +1,12 @@
 ﻿#include <memory_resource>
 
 #include <containers/vector.hpp>
+#include <containers/pmr_allocator.hpp>
+#include <containers/new_delete_pmr.hpp>
 
 #include "s_expr_int_calculator.h"
 
 int main() {
-    std::pmr::vector<int> vec{std::pmr::new_delete_resource()};
+    ml::vector<int, ml::pmr_allocator<int>> vec{ml::get_new_delete_pmr()};
     return 0;
 }
