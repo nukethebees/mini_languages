970c686c2e1ffbd1b5a5650ec439b93e68eef751
Matthew Love <matthewlove94@gmail.com>
2025-06-24 20:51:49 +0100

    Add more token cases

--- s_expr_int_calculator/s_expr_int_calculator/scanner.cpp
+++ s_expr_int_calculator/s_expr_int_calculator/scanner.cpp
@@ -13,7 +13,7 @@ auto scan_file(std::string_view file) -> vec<Token> {
     std::size_t offset{0};
     std::size_t length{file.length()};
 
-    auto get_lexeme{[&]() { return std::string_view{file.data() + offset, i - offset}; }};
+    auto get_lexeme{[&]() { return std::string_view{file.data() + i, offset}; }};
     auto get_idx{[&]() { return static_cast<FilePosition>(offset + i); }};
     auto is_eof{[&]() { return get_idx() >= length; }};
     auto get_char{[&]() {
@@ -24,7 +24,7 @@ auto scan_file(std::string_view file) -> vec<Token> {
             return file[idx];
         }
     }};
-    auto get_length{[&]() { return offset + 1; }};
+    auto get_length{[&]() { return offset; }};
     auto is_num{[](char c) -> bool { return (c <= '9') && (c >= '0'); }};
     auto is_alpha{[](char c) -> bool { return std::isalpha(static_cast<int>(c)); }};
     auto is_idenchar{
@@ -41,6 +41,7 @@ auto scan_file(std::string_view file) -> vec<Token> {
                 ++i;
                 break;
             }
+
             case '#': {
                 ++offset;
                 // Skip comments
@@ -51,8 +52,28 @@ auto scan_file(std::string_view file) -> vec<Token> {
             }
             case '\0': {
                 // End of file
+                ++offset;
                 out.emplace_back(i, get_length(), TokenType::END_OF_FILE, get_lexeme());
-                ++i;
+                break;
+            }
+            case '(': {
+                ++offset;
+                out.emplace_back(i, get_length(), TokenType::LEFT_PAREN, get_lexeme());
+                break;
+            }
+            case ')': {
+                ++offset;
+                out.emplace_back(i, get_length(), TokenType::RIGHT_PAREN, get_lexeme());
+                break;
+            }
+            case '+': {
+                ++offset;
+                out.emplace_back(i, get_length(), TokenType::PLUS, get_lexeme());
+                break;
+            }
+            case '-': {
+                ++offset;
+                out.emplace_back(i, get_length(), TokenType::MINUS, get_lexeme());
                 break;
             }
             default: {
@@ -60,11 +81,10 @@ auto scan_file(std::string_view file) -> vec<Token> {
                     do {
                         ++offset;
                     } while (is_num(get_char()));
-                    out.emplace_back(i,
-                                     get_length(),
-                                     TokenType::NUMBER,
-                                     get_lexeme(),
-                                     std::stoi(std::string(get_lexeme())));
+
+                    auto lex{get_lexeme()};
+                    out.emplace_back(
+                        i, get_length(), TokenType::NUMBER, lex, std::stoi(std::string(lex)));
                     break;
                 } else if (is_alpha(c)) {
                     do {
