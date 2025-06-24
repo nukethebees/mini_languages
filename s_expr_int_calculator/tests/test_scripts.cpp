8fc32d5fd8feb0c71bf1c82991928bd261a8752e
Matthew Love <matthewlove94@gmail.com>
2025-06-24 17:52:06 +0100

    Add empty test

--- s_expr_int_calculator/tests/test_scripts.cpp
+++ s_expr_int_calculator/tests/test_scripts.cpp
@@ -1,1 +1,23 @@
+#include <vector>
+
 #include "gtest/gtest.h"
+
+struct CodeGenTestInput {
+    std::string_view test_name;
+    std::string_view input;
+    std::string_view expected_output;
+};
+
+class CodeGenTest : public testing::TestWithParam<CodeGenTestInput> {};
+
+TEST_P(CodeGenTest, compile_file) {
+    auto const& param{GetParam()};
+    SUCCEED();
+}
+
+std::vector<CodeGenTestInput> const inputs{{{"empty", "", ""}}};
+
+INSTANTIATE_TEST_SUITE_P(CodeGenTests,
+                         CodeGenTest,
+                         testing::ValuesIn(inputs),
+                         [](auto const& info) { return std::string(info.param.test_name); });
